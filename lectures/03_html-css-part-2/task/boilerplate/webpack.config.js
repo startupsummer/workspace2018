a880563d0ad14dd6e19a2ff585ef05fef7264564
Mikhail Slizenko <stoleyourbicycle@gmail.com>
2018-01-24 11:58:55 +0300

    Update boilerplate in html-css-part-2 lecture

--- lectures/03_html-css-part-2/task/boilerplate/webpack.config.js
+++ lectures/03_html-css-part-2/task/boilerplate/webpack.config.js
@@ -1,12 +1,21 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReloadPlugin = require('reload-html-webpack-plugin');
+const path = require('path');
+
+const PATHS = {
+  src: path.join(__dirname, '/src'),
+  dist: path.join(__dirname,'/dist'),
+};
 
 module.exports = {
-  entry: `${__dirname}/prototype/assets/styles/index.pcss`,
+  context: PATHS.src,
+
+  entry: `./assets/styles/index.pcss`,
 
   output: {
-    path: `${__dirname}/build/`,
+    path: PATHS.dist,
     publicPath: '/',
     filename: 'bundle.js',
   },
@@ -33,15 +42,16 @@ module.exports = {
 
   devServer: {
     hot: true,
-    contentBase: `${__dirname}/`,
     publicPath: '/',
-    contentBase: path.join(__dirname, "prototype/assets"),
+    contentBase: path.join(__dirname, "src/assets"),
+    watchContentBase: true
   },
 
   devtool: 'source-map',
 
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({ template: './prototype/index.html', filename: 'index.html' }),
+    new HtmlWebpackPlugin({ template: './index.html', filename: 'index.html' }),
+    new ReloadPlugin(),
   ],
 };
