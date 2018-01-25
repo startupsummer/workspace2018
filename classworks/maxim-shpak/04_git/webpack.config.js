e4129426c68867fffda731d2796f6fdc5ed76b5b
Max-Starling <17.max.starling@gmail.com>
2018-01-25 11:58:26 +0300

    [Maxim Shpak] Solve task #4(Webpack)

---
+++ classworks/maxim-shpak/04_git/webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const SuperTrickyPlugin = require('./SuperTrickyPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const config = {
+  entry: './src/index.js',
+
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.pcss/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.js/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+
+  devtool: 'cheap-source-map',
+
+  resolve: {
+    extensions: ['.js', '.css','.pcss'],
+    modules: [
+      'node_modules',
+      path.resolve('./src')
+    ]
+  },
+
+  output: {
+    path: path.resolve('./build'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html')
+    }),
+    new SuperTrickyPlugin(),
+    new webpack.DefinePlugin({
+      'window.MY_NAME': JSON.stringify('Max Starling'),
+      'process.env':{  
+        'NODE_ENV': JSON.stringify('production')  
+      },
+    }),
+    new ExtractTextPlugin("styles.css"),
+  ]
+};
+
+module.exports = config;
