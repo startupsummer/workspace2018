406fc7cda2e4641f9303c4564beaa75cc766fc76
purpleow1 <purpleowllight@gmail.com>
2018-01-26 15:42:50 +0300

    [yk_05-javascript] Add timer function

--- classworks/yegor-koren/05_javascript/script/index.js
+++ classworks/yegor-koren/05_javascript/script/index.js
@@ -5,13 +5,15 @@ const buttons = {
   stop: document.querySelector('.stop.button'),
   plus: document.querySelector('.plus.button'),
   minus: document.querySelector('.minus.button'),
-  reset: document.querySelector('.reset.button')
+  reset: document.querySelector('.reset.button'),
+  reverse: document.querySelector('.reverse.button')
 };
 
 timer.textContent = '00:00';
 let time = 0,
     timeoutID,
-    flag = true;
+    flagStart = true,
+    flagReverce = true;
 
 function showTime() {
   let hours = Math.floor(time/60),
@@ -27,8 +29,10 @@ function showTime() {
   timer.textContent = hoursString + ":" + minutesString;
 }
 buttons.start.onclick = function() {
-  if (flag) {
-    flag = false;
+  if (flagStart) {
+    flagStart = false;
+    flagReverce = true;
+    clearTimeout(timeoutID);
     timeoutID = setTimeout(function tick() {
       time++;
       showTime();
@@ -38,7 +42,8 @@ buttons.start.onclick = function() {
 }
 buttons.stop.onclick = function() {
   clearTimeout(timeoutID);
-  flag = true;
+  flagStart = true;
+  flagReverce = true;
 }
 buttons.plus.onclick = function() {
   time += 10;
@@ -53,5 +58,24 @@ buttons.reset.onclick = function() {
   clearTimeout(timeoutID);
   time = 0;
   showTime();
-  flag = true;
+  flagStart = true;
+  flagReverce = true;
+}
+buttons.reverse.onclick = function() {
+  if (flagReverce) {
+    flagReverce = false;
+    flagStart = true;
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(function tick() {
+      time--;
+      if (time < 0) {
+        time = 0;
+        clearTimeout(timeoutID);
+        flagReverce = true;
+        return;
+      }
+      showTime();
+      timeoutID = setTimeout(tick, 1000);
+    }, 1000);
+  }
 }
