af08268ee4d37ecc42add4c293296244ddb65f6c
Evgeniy Marushko <32525473+nrt3@users.noreply.github.com>
2018-01-26 14:34:37 +0300

    Rebuild

--- classworks/evgeny-marushko/05_javascript/index.js
+++ classworks/evgeny-marushko/05_javascript/index.js
@@ -1,4 +1,4 @@
-const timer = document.querySelector('.timer');
+const field = document.querySelector('.timer');
 
 const buttons = {
   start: document.querySelector('.start.button'),
@@ -10,117 +10,86 @@ const buttons = {
   switchState: document.querySelector('.switch.button'),
 };
 
-timer.textContent = '00:00';
+field.textContent = '00:00';
 
-let secs = 0;
-let mins = 0;
-let interval;
-let isStarted = false;
-let state = 'stopwatch';
-
-let time = () => {
-	if (state == 'stopwatch') {
-		secs++;
-		if (secs == 60) {
-			secs = 0;
-			mins++;
+let timer = {
+	interval: null,
+	secs: 0,
+	isStarted: false,
+	state: 'stopwatch',
+	getMins: () => {
+		let result = parseInt(timer.secs/60);
+		if (result < 10) {
+			return '0' + result;
+		} else {
+			return result;
 		}
-	} else {
-		secs--;
-		if (secs == 0) {
-			if (mins > 0) {
-				secs = 59;
-				mins--;
-			} else {
-				stop();
-				alert('Время вышло!');
-			} 
+	},
+	getSecs: () => {
+		let result = timer.secs % 60;
+		if (result < 10) {
+			return '0' + result;
+		} else {
+			return result;
 		}
-	}
-	updateTime();
-};
-
-let updateTime = () => {
-	let textSecs;
-	let textMins;
-	if (secs < 10) {
-		textSecs = '0' + secs;
-	} else {
-		textSecs = secs;
-	}
-	if (mins < 10) {
-		textMins = '0' + mins;
-	} else {
-		textMins = mins;
-	}
-	timer.textContent = textMins + ':' + textSecs;
-};
-
-let start = () => {
-	if (!isStarted) {
-		interval = setInterval(time, 1000);
-		isStarted = true;
-	}
-}
-
-let stop = () => {
-	if (isStarted) {
-		window.clearInterval(interval);
-		isStarted = false;
-	}
-}
-
-let reset = () => {
-	mins = 0;
-	secs = 0;
-	stop();
-	updateTime();
-}
-
-let plusSecs = () => {
-	if (secs < 50) {
-		secs+= 10;
-	} else {
-		mins++;
-		secs = secs % 50;
-	}
-	updateTime();
-};
-
-let munisSecs = () => {
-	if (secs >= 10) {
-		secs-= 10;
-	} else {
-		if (mins > 0) {
-			secs = (10 - secs) + 50;
-			if (secs == 60) { //bug fix
-				secs = 0;
+	},
+	toField: () => {
+		field.textContent = timer.getMins() + ':' + timer.getSecs();
+	},
+	tick: () => {
+		if (timer.state == 'stopwatch') {
+			timer.secs++;
+		} else {
+			timer.secs--;
+			if (timer.secs == 0) {
+				timer.stop();
 			}
-			mins--;
 		}
-	}
-	updateTime();
-};
-
-let addResult  = () => {
-	results.innerHTML += `<li class="result">${timer.textContent}</li>`;
-};
-
-let switchState = () => {
-	if (state == 'timer') {
-		state = 'stopwatch';
-	} else {
-		state = 'timer';
+		timer.toField();
+	},
+	start: () => {
+		if (!timer.isStarted) {
+			timer.interval = setInterval(timer.tick, 1000);
+			timer.isStarted = true;
+		};
+	},
+	stop: () => {
+		if (timer.isStarted) {
+			window.clearInterval(timer.interval);
+			timer.isStarted = false;
+		};
+	},
+	plus: () => {
+		timer.secs += 10;
+		timer.toField();
+	},
+	minus: () => {
+		if (timer.secs >= 10) {
+			timer.secs -= 10;
+			timer.toField();
+		}
+	},
+	addResult: () => {
+		results.innerHTML += `<li class="result">${field.textContent}</li>`;
+	},
+	switchState: () => {
+		if (timer.state == 'timer') {
+			timer.state = 'stopwatch';
+		} else {
+			timer.state = 'timer';
+		}
+	},
+	reset: () => {
+		timer.secs = 0;
+		timer.toField();
 	}
 }
 
-buttons.plus.addEventListener('click', plusSecs);
-buttons.minus.addEventListener('click', munisSecs);
-buttons.start.addEventListener('click', start);
-buttons.stop.addEventListener('click', stop);
-buttons.reset.addEventListener('click', reset);
+buttons.plus.addEventListener('click', timer.plus);
+buttons.minus.addEventListener('click', timer.minus);
+buttons.start.addEventListener('click', timer.start);
+buttons.stop.addEventListener('click', timer.stop);
+buttons.reset.addEventListener('click', timer.reset);
 
-buttons.addResult.addEventListener('click', addResult);
-buttons.switchState.addEventListener('click', switchState);
-
-
+buttons.addResult.addEventListener('click', timer.addResult);
+buttons.switchState.addEventListener('click', timer.switchState);
