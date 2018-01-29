cd1ccc067709842a9dd1c4931bd17023a9064c76
Anna K <ann.korostelev@gmail.com>
2018-01-29 18:31:26 +0300

    Implement task4

---
+++ classworks/anna_korosteleva/04_webpack/webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const SuperTrickyPlugin = require('./SuperTrickyPlugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin")
+
+const config = {
+  entry: './src/index.js',
+
+  module: {
+    rules: [
+      {
+	test: /\.p?css/,
+	use: ExtractTextWebpackPlugin.extract({
+		fallback: 'style-loader',
+		use: ['css-loader', 'postcss-loader']
+      	})
+	}, {
+	test: /\.js/,
+	use: ['babel-loader'],
+      }
+]
+  },
+
+  devtool: 'cheap-source-map',
+
+  resolve: {
+    extensions: ['.js', '.css','.pcss'],
+    modules: [
+      'node_modules',
+      path.resolve('./src')
+    ]
+  },
+
+  output: {
+    path: path.resolve('./build'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'window.MY_NAME': JSON.stringify('ANNA'),
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html')
+    }),
+    new SuperTrickyPlugin(),
+    new ExtractTextWebpackPlugin("styles.css")
+  ]
+}
+
+module.exports = config
