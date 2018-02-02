fe411401b0c217597ecb636f356e2e5adfe36854
Polina <35339540+Rudenkopolina@users.noreply.github.com>
2018-02-02 11:08:42 +0300

    Implement 07_react_part1 task
    * [Rudenko_Polina]07_react_part1
    
    * create issues
    
    * create issues
    
    * optimisatefunctions
    
    * update task 8
    
    * Complete git integration

---
+++ classworks/polina-rudenko/07_react/my-app/src/components/issues-listing/IssuesListing.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './issuesListing.styles.css';
+import Subnav from '../subnav/Subnav';
+import Issues from '../issues/Issues';
+import States from '../states/States';
+
+
+class IssuesListing extends Component {
+  state = {
+  }
+
+  render() {
+    return (
+      <div className="issues-listing">
+        <div className="issues-listing__subnav">
+          <Subnav
+            addNewIssue={this.props.addNewIssue}
+          />
+        </div>
+        <div className="issues-listing__header">
+          <States
+            countOpen={this.props.countOpen}
+            countClose={this.props.countClose}
+            onClickClose={this.props.onClickClose}
+            onClickOpen={this.props.onClickOpen}
+          />
+        </div>
+        <div className="issues-listing__body">
+          <Issues
+            switchIssueStatus={this.props.switchIssueStatus}
+            issuesArr={this.props.issuesArr}
+            issuesStatus={this.props.issuesStatus}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+IssuesListing.propTypes = {
+  issuesStatus: PropTypes.string.isRequired,
+  onClickClose: PropTypes.func.isRequired,
+  onClickOpen: PropTypes.func.isRequired,
+  addNewIssue: PropTypes.func.isRequired,
+  countOpen: PropTypes.func.isRequired,
+  countClose: PropTypes.func.isRequired,
+  switchIssueStatus: PropTypes.func.isRequired,
+  issuesArr: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.oneOf(['open', 'closed']).isRequired,
+  }).isRequired).isRequired,
+};
+
+export default IssuesListing;
