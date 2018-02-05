e0796396ff378589e965c76dd76e2b1fe3cc6bdc
kazeens <kolyanbestele@gmial.com>
2018-02-05 13:22:22 +0300

    Remake code

--- classworks/nicolay-yurkevich/10_node/index.js
+++ classworks/nicolay-yurkevich/10_node/index.js
@@ -1,40 +1,45 @@
 var
-http = require('http'),
-path = require('path'),
-url= require('url');
-fs = require('fs');
- 
+    http = require('http'),
+    path = require('path'),
+    url = require('url');
+	fs = require('fs');
+
 function requestHandler(req, res) {
-	var content = '';
- 	var queryData = url.parse(req.url, true).query;
- 	var pathname = url.parse(req.url).pathname;
- 	console.log(queryData);
- 	console.log(pathname);
+    var content = '';
+    var queryData = url.parse(req.url, true).query;
+    var pathname = url.parse(req.url).pathname;
+    console.log(queryData);
+    console.log(pathname);
 
-	if(pathname === '/'){
-		let cont;
-		fs.readFile('./test.html','utf8',function(err,contents){
-		if(err){
-		  throw new Error();
-		}
-	     cont=contents
-		});
-		res.write(cont);	
-	    res.end();
-	}
-	if(pathname==='/Hello'){
-		res.write("Hello, World");
-	    res.end();
-	}
-	if(queryData.name!=null){
-		res.writeHead(200, {"Content-Type": "text/plain"});
-		res.end('Hello ' + queryData.name + '\n');
-	}
+    if (pathname === '/') {
 
-	else {
-		res.writeHead(404, {'Content-Type': 'text/html'});
-		res.end('<h1>Sorry, the page you are looking for cannot be found.</h1>');
-	};
-};
+        let cont;
+        fs.readFile('./test.html', 'utf8', function(err, contents) {
+            if (err) {
+                throw err;
+            }
+            res.end(contents);
+        });
+
+    } else {
+        if (pathname === '/Hello') {
+            if (queryData.name) {
+                res.writeHead(200, {
+                    "Content-Type": "text/plain"
+                });
+                res.end('Hello ' + queryData.name + '\n');
+            } else {
+                res.write("Hello, World");
+                res.end();
+            }
+        } else {
+            res.writeHead(404, {
+                'Content-Type': 'text/html'
+            });
+            res.end('<h1>Sorry, the page you are looking for cannot be found.</h1>');
+        }
+    }
+}
+
 
 http.createServer(requestHandler).listen(1212);
