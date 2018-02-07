9fe6565a47fc3c5320aa543f191a4fd054d8620e
Evgeniy Marushko <32525473+nrt3@users.noreply.github.com>
2018-02-07 16:18:03 +0300

    Add task

---
+++ classworks/evgeny-marushko/12_api/src/app.js
@@ -0,0 +1,52 @@
+const Koa = require('koa');
+const jwt = require('jsonwebtoken');
+const kjwt = require('koa-jwt');
+const Router = require('koa-router');
+const Joi = require('joi');
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+const router = new Router();
+
+const schema = Joi.object().keys({
+  email: Joi.string().email(),
+  password: Joi.string().min(6).max(30)
+});
+
+app.use(bodyParser());
+router
+  .post('/api/v1/account/login', (ctx, next) => {
+    console.log(jwt.sign({field: "pls"}, 'secret'));
+    ctx.body = ctx.request.body;
+    const result = Joi.validate(ctx.request.body, schema);
+    if (result.error != null) {
+      const errArray = [];
+      result.error.details.forEach(item => errArray.push(item.message));
+      ctx.body = errArray;
+      ctx.status = 400;
+    }
+    ctx.body = new String(jwt.sign(ctx.request.body, 'secret'));
+  })
+  .post('/api/v1/test', (ctx, next) => {
+    const token =
+      ctx.request.body.token ||
+      ctx.query.token ||
+      ctx.request.headers['x-access-token'];
+    jwt.verify(token, 'secret', function (err, decoded) {
+      if (err) {
+        ctx.body = { error: err.message };
+      } else {
+        ctx.body = decoded;
+      }
+    })
+  });
+
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+
+
+app.listen(3000);
