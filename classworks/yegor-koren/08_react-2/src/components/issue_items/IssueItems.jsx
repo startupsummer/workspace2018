361c4ad57a3d763c34b12d075516d0288212e6ad
purpleow1 <purpleowllight@gmail.com>
2018-02-11 17:46:31 +0300

    [yk_react-2] Initial commit

---
+++ classworks/yegor-koren/08_react-2/src/components/issue_items/IssueItems.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../button/Button';
+import './issue_items.style.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+const IssueItems = ({
+  changeIssue,
+  issues,
+  filter,
+}) => {
+  const buttonName = filter === 'open' ? 'Close issue' : 'Open issue';
+  const icon = filter === 'open' ?
+    (
+      <div className="issues__status issues__status--open">
+        <svg className="issues__icon" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path fillRule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z" /></svg>
+      </div>
+    ) :
+    (
+      <div className="issues__status issues__status--closed">
+        <svg aria-hidden="true" className=" issues__icon" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path fillRule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5z" /></svg>
+      </div>
+    );
+  const issuesList = issues.map(item => (
+    <li className="issues__item" key={issues.id} >
+      {icon}
+      <div className="issues__title">
+        <a href="#" className="issues__link">
+          {item.title}
+        </a>
+      </div>
+      <Button
+        action={changeIssue}
+        id={item.id}
+      >{buttonName}
+      </Button>
+    </li>
+  ));
+
+  return (
+    <ul className="issues" >
+      {issuesList}
+    </ul>
+  );
+};
+
+IssueItems.propTypes = {
+  changeIssue: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  issues: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default IssueItems;
---
+++ classworks/yegor-koren/08_react-2/src/components/main/Main.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import PageHead from '../page_head/PageHead';
+import Subnav from '../subnav/Subnav';
+import IssuesList from '../issues_list/IssuesList';
+import './main.style.css';
+
+class Main extends React.PureComponent {
+  render() {
+    const issuesNumber = this.props.issues.filter(item => item.state === 'open').length;
+
+    return (
+      <div className="content">
+        <PageHead
+          issuesNumber={issuesNumber}
+        />
+        <Subnav
+          newIssue={this.props.newIssue}
+          changeFilterSearch={this.props.changeFilterSearch}
+        />
+        <IssuesList
+          changeFilter={this.props.changeFilter}
+          changeIssue={this.props.changeIssue}
+          filter={this.props.filter}
+          issues={this.props.issues}
+          filterSearch={this.props.filterSearch}
+        />
+      </div>
+    );
+  }
+}
+
+Main.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+  newIssue: PropTypes.func.isRequired,
+  changeIssue: PropTypes.func.isRequired,
+  changeFilterSearch: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  issues: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  })).isRequired,
+  filterSearch: PropTypes.string.isRequired,
+};
+
+export default Main;
