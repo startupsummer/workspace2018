c643f1c01f86ff5742294b770a70e2ba010e64df
Shved <snuggleshved@gmail.com>
2018-02-19 19:05:48 +0300

    [ISSUE-15] - Add resolving task

---
+++ classworks/eugene-shved/15_react-native/src/Layout.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import faker from 'faker';
+
+import { View } from 'react-native';
+import styles from './Layout.styles';
+
+import Header from './components/Header';
+import Content from './components/Content';
+
+class Layout extends React.Component {
+  state = {
+    data: [],
+    query: '',
+  }
+
+  componentDidMount() {
+    const newData = Array.from(Array(20)).map((item, index) => ({
+      id: index + 1,
+      title: faker.name.findName(),
+      text: faker.name.jobTitle(),
+      image: faker.image.avatar(),
+      star: false,
+    }));
+    this.setState({ data: newData });
+  }
+
+  setActualData = (query) => {
+    this.setState({ query });
+  }
+
+  setToFavorites = (id) => {
+    let newData = this.state.data
+    newData[id].star = ! newData[id].star
+    this.setState({ data: newData })
+  }
+
+  render() {
+    const { data, query } = this.state;
+
+    const actualData = data.sort((a, b) => {
+      return (a.id - b.id);
+    })
+    .sort((a) => {
+      return (a.star === true) ? -1 : 1;
+    })
+    .filter(item =>
+      item.title.toLowerCase().includes(query.toLowerCase()));
+
+    return (
+      <View style={styles.container}>
+        <Header filtering={this.setActualData}/>
+        <Content data={actualData} toFavoritesFunction={this.setToFavorites}/>
+      </View>
+    );
+  }
+}
+
+export default Layout;
