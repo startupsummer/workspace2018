197590a39c5642da6bdf79b3b0bc9da12a20e25c
Anna K <ann.korostelev@gmail.com>
2018-02-21 16:43:41 +0300

    Fix bug

--- classworks/anna_korosteleva/10_node/server.js
+++ classworks/anna_korosteleva/10_node/server.js
@@ -1,15 +1,17 @@
 const http = require('http');
 const fs = require('fs');
+const parse = require('url-parse');
 
 const index = fs.readFileSync('./index.html', 'utf8');
 
-http.createServer(function(request, response) {
-    if (request.url === '/') {
+http.createServer(function (request, response) {
+  url = parse(request.url, true);
+  switch (url.href) {
+    case '/':
       response.end(index);
-    } else if( request.url.includes('/hello')) {
-      const name = request.url.replace('/hello?name=', '');
-      if (name === '/hello'){
-        response.end('Hello, world!');
-      }else response.end(`Hello, ${name}!`);
-    }
+    case '/hello':
+      response.end('Hello, world!');
+    default:
+      response.end(`Hello, ${url.query.name}!`);
+  }
 }).listen(5000);
