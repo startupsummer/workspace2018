1a42d4f61a8a99a866659ae68fa6b521271acd9d
ники <nikkkdance@mail.ru>
2018-03-11 03:49:37 +0300

    Add task folder

---
+++ classworks/Nikita-Goncharov/11_koa/webpack.config.js
@@ -0,0 +1,69 @@
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = {
+  entry: {
+    'index':['./src/client/index.js'],
+    'reviews':['./src/client/reviews.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.pcss/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+
+  devtool: 'cheap-source-map',
+
+  resolve: {
+    extensions: ['.js', '.css','.pcss'],
+    modules: [
+      'node_modules',
+      path.resolve('./src')
+    ]
+  },
+
+  output: {
+    path: path.join(__dirname, './public'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new webpack.DefinePlugin({
+      'window.MY_NAME':'"Nikita"',
+      'process.env.NODE_ENV':'"production"'
+    }),
+
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ["index"],
+      filename: "index.html",
+      template: "./src/client/index.html"
+    }),
+
+    new HtmlWebpackPlugin({
+      inject: false,
+      chunks: ["reviews"],
+      filename: "reviews.html",
+      template: "./src/client/reviews.html"
+    }),
+
+  ]
+}
+
+module.exports = config
