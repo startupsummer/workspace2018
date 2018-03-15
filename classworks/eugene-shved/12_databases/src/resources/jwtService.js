ab952fab792f78705ff157773f85c1a6abbbb831
Shved <snuggleshved@gmail.com>
2018-03-15 22:02:05 +0300

    Add resolution task

---
+++ classworks/eugene-shved/12_databases/src/resources/jwtService.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken');
+
+const config = require('../../config/index');
+
+module.exports = {
+    generateToken: data => jwt.sign(data, config.secretKey),
+    verifyUser: token => jwt.verify(token, config.secretKey,
+      (error, data) => error ? null : data),
+};
