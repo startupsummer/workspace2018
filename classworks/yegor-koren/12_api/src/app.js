15c9163e1e1e1eac9f99ba0e6903284044608788
purpleow1 <purpleowllight@gmail.com>
2018-03-20 18:49:43 +0300

    Add controller

--- classworks/yegor-koren/12_api/src/app.js
+++ classworks/yegor-koren/12_api/src/app.js
@@ -2,9 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
-const validation = require('./utils/validation');
-const auth = require('./utils/auth');
-const jwt = require('./utils/jwt');
+const controller = require('./resources/controller');
 const config = require('../config');
 
 const app = new Koa();
@@ -13,20 +11,8 @@ const router = new Router();
 app.use(bodyParser());
 
 router
-  .post('/api/v1/account/login', async (ctx, next) => {
-    const valid = validation(ctx.request.body);
-    ctx.assert(!valid.error, 400);
-
-    if (auth(valid.value)) {
-        const token = jwt.createToken(valid.value);
-        ctx.body = token;
-    }
-  })
-  .get('/api/v1/me', async (ctx, next) => {
-    const decoded = jwt.verifyToken(ctx.headers['access-token']);
-    if (decoded !== null) ctx.body = decoded;
-    else ctx.status = 401;
-  });
+  .post('/api/v1/account/login', controller.login)
+  .get('/api/v1/me', controller.getMe);
 
   app
     .use(router.routes())
